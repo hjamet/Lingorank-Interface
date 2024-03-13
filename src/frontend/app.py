5af59e1af1d4baf27f73988f5cf0132fa10887d4
Henri Jamet <henri.jamet@epita.fr>
2024-03-13 22:56:28 +0100

    feat: :tada: Hello World

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,6 +1,7 @@
 from typing import Any
 import flask
 import dash
+import dash_mantine_components as dmc
 
 
 class App:
@@ -10,9 +11,32 @@ class App:
 
         # Create the Dash app
         self.dash_app = dash.Dash(
-            __name__, server=self.flask_app, url_base_pathname="/"
+            __name__,
+            server=self.flask_app,
+            url_base_pathname="/",
         )
 
+        # Set the layout
+        self.dash_app.layout = self.__get_layout()
+
     def __call__(self, debug: bool = False, port: int = 5000):
-        """Run the server"""
+        """Run the server
+
+        Args:
+            debug (bool, optional): Whether to run the server in debug mode. Defaults to False.
+            port (int, optional): The port to run the server on. Defaults to 5000.
+        """
+        # Run the server
         self.dash_app.run_server(debug=debug, port=port)
+
+    def __get_layout(self):
+        """An internal method to get the layout of the app.
+
+        Returns:
+            str: The layout of the app.
+        """
+        # Create Hello World
+        hello_world = dmc.Title("Hello, World!")
+
+        # Return the layout
+        return hello_world
